eb4ed1777ff950e53a6d89bf0ff226f4ace80d24
kodonnell <kodonnell@users.noreply.github.com>
2016-10-05 13:41:48 +1300

    initial work based on graphhopper Measurement.java

--- map-matching.sh
+++ map-matching.sh
@@ -1,51 +1,86 @@
 #!/bin/bash
 
+# bail if any errors ...
+set -e
+
 if [ "$JAVA_HOME" != "" ]; then
- JAVA=$JAVA_HOME/bin/java
+    JAVA=$JAVA_HOME/bin/java
 fi
 
 if [ "$JAVA" = "" ]; then
- JAVA=java
+    JAVA=java
 fi
 
 echo "using $JAVA"
 
+function set_jar {
+    local module=$1
+    local pattern="matching-$module/target/graphhopper-map-matching-*-dependencies.jar"
+    if ! ls $pattern > /dev/null 2>&1; then
+        mvn --projects hmm-lib -DskipTests=true install
+        mvn --projects matching-$module,matching-core -DskipTests=true install assembly:single
+    fi
+    JAR=$(ls matching-$module/target/graphhopper-map-matching-*-dependencies.jar)
+}
+
 if [ "$1" = "action=start-server" ]; then
-  function set_jar_path {
-    JAR=$(ls matching-web/target/graphhopper-map-matching-web-*-dependencies.jar)
-  }
-
-  set_jar_path
-
-  if [ ! -f "$JAR" ]; then
-    mvn --projects hmm-lib -DskipTests=true install      
-    mvn --projects matching-web,matching-core -DskipTests=true install assembly:single
-    set_jar_path
-  fi
-  
-  ARGS="graph.location=./graph-cache jetty.resourcebase=matching-web/src/main/webapp"
-  
+    set_jar "web"
+    ARGS="graph.location=./graph-cache jetty.resourcebase=matching-web/src/main/webapp"
 elif [ "$1" = "action=test" ]; then
+    export MAVEN_OPTS="-Xmx400m -Xms400m"
+    mvn clean test verify
+    # return exit code of mvn
+    exit $?
+elif [ "$1" = "action=measurement" ]; then
+    set_jar "tools"
+    fname="measurement.$(date +%Y%m%d_%H%M%S)"
+    ARGS="config=$CONFIG graph.location=$GRAPH datareader.file=$2 prepare.ch.weightings=fastest graph.flag_encoders=car prepare.min_network_size=10000 prepare.min_oneway_network_size=10000"
+    current_commit=$(git log -n 1 --pretty=oneline | cut -d' ' -f1)
+    function startMeasurement {
+        commit_info=$(git log -n 1 --pretty=oneline)
+        echo -e "\nperforming measurement for commit $current_commit"
+        "$JAVA" $JAVA_OPTS -cp "$JAR" com.graphhopper.matching.tools.Measurement $ARGS measurement.count=5000 measurement.location="$measurement_fname" measurement.gitinfo="$commit_info"
+    }
 
-  export MAVEN_OPTS="-Xmx400m -Xms400m"
-  mvn clean test verify
-  # return exit code of mvn
-  exit $?
-  
+    # use all <last_commits> versions starting from HEAD
+    last_commits=$3
+    if [ -z "$last_commits" ]; then
+        measurement_fname=$fname
+        tested_commit=$current_commit
+        startMeasurement
+    else
+        echo "commits (in order tested):" >> $fname
+        # TODO: check changes are committed?
+        values=$fname.values
+        measurement_fname=$fname.tmp
+        commits=$(git rev-list HEAD -n $last_commits)
+        first=true
+        for commit in $commits; do
+            git checkout $commit -q
+            git log -n 1 --pretty=oneline >> $fname
+            mvn --projects matching-tools -DskipTests=true clean install assembly:single
+            rm -f $measurement_fname
+            startMeasurement
+            while read line; do
+                key=${line%%=*}
+                value=$(printf "%-10s" ${line##*=})
+                if [ "$first" = true ] ; then
+                    echo $key$value >> $values
+                else
+                    sed -ir "s/($key.*)/\1$value/g" $values
+                fi
+            done < $measurement_fname
+        done
+        echo -e "\nmeasurements:\n-------------\n" >> $fname
+        cat $values >> $fname
+        rm $values
+        rm $measurement_fname
+        # revert checkout
+        git checkout $current_commit
+    fi
 else
-  function set_jar_path {
-    JAR=$(ls matching-core/target/graphhopper-map-matching-*-dependencies.jar)
-  }
-
-  set_jar_path
-
-  if [ ! -f "$JAR" ]; then
-    mvn --projects hmm-lib -DskipTests=true install
-    mvn --projects matching-core -DskipTests=true install assembly:single
-    set_jar_path
-  fi
-  
-  ARGS="$@"
+    set_jar "core"
+    ARGS="$@"
 fi
 
 exec "$JAVA" $JAVA_OPTS -jar $JAR $ARGS prepare.min_network_size=0 prepare.min_one_way_network_size=0
---
+++ matching-tools/src/main/java/com/graphhopper/matching/tools/Measurement.java
@@ -0,0 +1,228 @@
+/*
+ *  Licensed to GraphHopper GmbH under one or more contributor
+ *  license agreements. See the NOTICE file distributed with this work for 
+ *  additional information regarding copyright ownership.
+ * 
+ *  GraphHopper GmbH licenses this file to you under the Apache License, 
+ *  Version 2.0 (the "License"); you may not use this file except in 
+ *  compliance with the License. You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.graphhopper.matching.tools;
+
+import com.graphhopper.GHRequest;
+import com.graphhopper.GHResponse;
+import com.graphhopper.GraphHopper;
+import com.graphhopper.PathWrapper;
+import com.graphhopper.coll.GHBitSet;
+import com.graphhopper.matching.LocationIndexMatch;
+import com.graphhopper.matching.MapMatching;
+import com.graphhopper.matching.MatchResult;
+import com.graphhopper.reader.osm.GraphHopperOSM;
+import com.graphhopper.routing.util.*;
+import com.graphhopper.routing.weighting.Weighting;
+import com.graphhopper.storage.Graph;
+import com.graphhopper.storage.GraphHopperStorage;
+import com.graphhopper.storage.index.LocationIndexTree;
+import com.graphhopper.util.*;
+import com.graphhopper.util.shapes.BBox;
+import com.graphhopper.util.shapes.GHPoint;
+import com.graphhopper.util.shapes.GHPoint3D;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.TreeMap;
+
+/**
+ * @author Peter Karisch
+ * @author kodonnell
+ */
+public class Measurement {
+    private static final Logger logger = LoggerFactory.getLogger(Measurement.class);
+    private final Map<String, String> properties = new TreeMap<String, String>();
+    private long seed;
+    private int count;
+
+    public static void main(String[] strs) {
+        new Measurement().start(CmdArgs.read(strs));
+    }
+
+    // creates properties file in the format key=value
+    // Every value is one y-value in a separate diagram with an identical x-value for every Measurement.start call
+    void start(CmdArgs args) {
+        String graphLocation = args.get("graph.location", "");
+        String propLocation = args.get("measurement.location", "");
+        if (Helper.isEmpty(propLocation))
+            propLocation = "measurement" + new SimpleDateFormat("yyyy-MM-dd_HH_mm_ss").format(new Date()) + ".properties";
+
+        seed = args.getLong("measurement.seed", 123);
+        String gitCommit = args.get("measurement.gitinfo", "");
+        count = args.getInt("measurement.count", 5000);
+
+        GraphHopper hopper = new GraphHopperOSM();
+        hopper.init(args).forDesktop();
+        hopper.getCHFactoryDecorator().setDisablingAllowed(true);
+        hopper.importOrLoad();
+        GraphHopperStorage g = hopper.getGraphHopperStorage();
+        String vehicleStr = args.get("graph.flag_encoders", "car");
+        FlagEncoder encoder = hopper.getEncodingManager().getEncoder(vehicleStr);
+        Weighting weighting = hopper.getCHFactoryDecorator().getWeightings().get(0);
+        
+
+        GraphHopperStorage graph = hopper.getGraphHopperStorage();
+        LocationIndexMatch locationIndex = new LocationIndexMatch(graph, (LocationIndexTree) hopper.getLocationIndex());
+        MapMatching mapMatching = new MapMatching(graph, locationIndex, encoder);
+        
+        StopWatch sw = new StopWatch().start();
+        try {
+//            maxNode = g.getNodes();
+//            GHBitSet allowedEdges = printGraphDetails(g, vehicleStr);
+            boolean isCH = false;
+            printLocationIndexMatchQuery(g, locationIndex);
+            printTimeOfMapMatchQuery(hopper, mapMatching, g);
+//            printMiscUnitPerfTests(g, isCH, encoder, count * 100, allowedEdges);
+//            printLocationIndexQuery(g, hopper.getLocationIndex(), count);
+
+//            printTimeOfRouteQuery(hopper, isCH, count / 20, "routing", vehicleStr, true);
+
+            System.gc();
+
+//            CHGraph lg = g.getGraph(CHGraph.class, weighting);
+//            fillAllowedEdges(lg.getAllEdges(), allowedEdges);
+//            isCH = true;
+//            printMiscUnitPerfTests(lg, isCH, encoder, count * 100, allowedEdges);
+//            printTimeOfRouteQuery(hopper, isCH, count, "routingCH", vehicleStr, true);
+//            printTimeOfRouteQuery(hopper, isCH, count, "routingCH_no_instr", vehicleStr, false);
+            logger.info("store into " + propLocation);
+        } catch (Exception ex) {
+            logger.error("Problem while measuring " + graphLocation, ex);
+            put("error", ex.toString());
+        } finally {
+            put("measurement.gitinfo", gitCommit);
+            put("measurement.count", count);
+            put("measurement.seed", seed);
+            put("measurement.time", sw.stop().getTime());
+            System.gc();
+            put("measurement.totalMB", Helper.getTotalMB());
+            put("measurement.usedMB", Helper.getUsedMB());
+            try {
+                store(new FileWriter(propLocation), "measurement finish, "
+                        + new Date().toString() + ", " + Constants.BUILD_DATE);
+            } catch (IOException ex) {
+                logger.error("Problem while storing properties " + graphLocation + ", " + propLocation, ex);
+            }
+        }
+    }
+
+    
+    private void printLocationIndexMatchQuery(Graph g, final LocationIndexMatch idx) {
+        final BBox bbox = g.getBounds();
+        final double latDelta = bbox.maxLat - bbox.minLat;
+        final double lonDelta = bbox.maxLon - bbox.minLon;
+        final Random rand = new Random(seed);
+        MiniPerfTest miniPerf = new MiniPerfTest() {
+            @Override
+            public int doCalc(boolean warmup, int run) {
+                double lat = rand.nextDouble() * latDelta + bbox.minLat;
+                double lon = rand.nextDouble() * lonDelta + bbox.minLon;
+                int val = idx.findNClosest(lat, lon, EdgeFilter.ALL_EDGES, rand.nextDouble() * 500).size();
+                return val;
+            }
+        }.setIterations(count).start();
+        print("location_index_match", miniPerf);
+    }
+
+    private void printTimeOfMapMatchQuery(final GraphHopper hopper, final MapMatching mapMatching, Graph g) {
+    	
+    	// pick random endpoints to create a route, then pick random points from the route,
+    	// and then run the random points through map-matching.
+    	final BBox bbox = g.getBounds();
+        final double latDelta = bbox.maxLat - bbox.minLat;
+        final double lonDelta = bbox.maxLon - bbox.minLon;
+        final Random rand = new Random(seed);
+        DistanceCalcEarth distCalc = new DistanceCalcEarth();
+        mapMatching.setMaxVisitedNodes((int) 1e10); 
+        MiniPerfTest miniPerf = new MiniPerfTest() {
+            @Override
+            public int doCalc(boolean warmup, int run) {
+            	boolean foundPath = false;
+            	while (!foundPath) {
+		            double lat0 = rand.nextDouble() * latDelta + bbox.minLat;
+		            double lon0 = rand.nextDouble() * lonDelta + bbox.minLon;
+		            double lat1 = rand.nextDouble() * latDelta + bbox.minLat;
+		            double lon1 = rand.nextDouble() * lonDelta + bbox.minLon;
+		            double sampleProportion = rand.nextDouble();
+		            GHResponse r = hopper.route(new GHRequest(lat0, lon0, lat1, lon1));
+		            if (!r.hasErrors()) {
+		            	foundPath = true;
+		            	long t = 0;
+		                List<GPXEntry> mock = new ArrayList<GPXEntry>();
+		                GHPoint prev = null;
+		                PointList points = r.getBest().getPoints();
+		            	for (GHPoint p : points) {
+		            		if (null != prev && rand.nextDouble() < sampleProportion) {
+		            			double dx = distCalc.calcDist(prev.lat, prev.lon, p.lat, p.lon);
+		            			double speedKPH = rand.nextDouble() * 100;
+		            			double dt = (dx / 1000) / speedKPH * 3600000;
+		            			t += (long) dt;
+		            			mock.add(new GPXEntry(p, t));
+		            		}
+		            	}		            	
+		            	// now match:
+		            	if (mock.size() > 2) {
+		            		MatchResult match = mapMatching.doWork(mock);		            		
+		            	}
+		            	// return something else?
+		            	return 0;
+		            }		            
+            	}
+				return 0;
+            }
+        }.setIterations(count).start();
+        print("map_match", miniPerf);
+    	
+    }
+ 
+
+    void print(String prefix, MiniPerfTest perf) {
+        logger.info(prefix + ": " + perf.getReport());
+        put(prefix + ".sum", perf.getSum());
+//        put(prefix+".rms", perf.getRMS());
+        put(prefix + ".min", perf.getMin());
+        put(prefix + ".mean", perf.getMean());
+        put(prefix + ".max", perf.getMax());
+    }
+
+    void put(String key, Object val) {
+        // convert object to string to make serialization possible
+        properties.put(key, "" + val);
+    }
+
+    private void store(FileWriter fileWriter, String comment) throws IOException {
+        fileWriter.append("#" + comment + "\n");
+        for (Entry<String, String> e : properties.entrySet()) {
+            fileWriter.append(e.getKey());
+            fileWriter.append("=");
+            fileWriter.append(e.getValue());
+            fileWriter.append("\n");
+        }
+        fileWriter.flush();
+    }
+}
