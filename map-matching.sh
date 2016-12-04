7cb64783b498531e88d53029c01b03d642fbedd7
kodonnell <kodonnell@users.noreply.github.com>
2016-12-04 15:33:59 +1300

    removed separate tools module and set name same as GH

--- map-matching.sh
+++ map-matching.sh
@@ -32,63 +32,63 @@ elif [ "$1" = "action=test" ]; then
     # return exit code of mvn
     exit $?
 elif [ "$1" = "action=measurement" ]; then
-    set_jar "tools"
-    fname="measurement.$(date +%Y%m%d_%H%M%S)"
+    set_jar "core"
     ARGS="config=$CONFIG graph.location=$GRAPH datareader.file=$2 prepare.ch.weightings=fastest graph.flag_encoders=car prepare.min_network_size=10000 prepare.min_oneway_network_size=10000"
     current_commit=$(git log -n 1 --pretty=oneline | cut -d' ' -f1)
     function startMeasurement {
         echo -e "\nperforming measurement for commit $current_commit"
-        mvn --projects matching-tools -DskipTests=true clean install assembly:single
-        rm -f "$measurement_fname"
-        "$JAVA" $JAVA_OPTS -cp "$JAR" com.graphhopper.matching.tools.Measurement $ARGS measurement.location="$measurement_fname"
+        mvn --projects matching-core -DskipTests=true clean install assembly:single
+        measurement_fname="measurement$(date +%Y-%m-%d_%H_%M_%S).properties"
+        "$JAVA" $JAVA_OPTS -cp "$JAR" com.graphhopper.matching.util.Measurement $ARGS measurement.location="$measurement_fname"
     }
 
     # use all <last_commits> versions starting from HEAD
     last_commits=$3
     if [ -z "$last_commits" ]; then
-        measurement_fname=$fname
         startMeasurement
     else
         # the following checks out the last commits, and tests each one. The code looks a bit
         # messier than that, as we merge the results into a single file (to make it easier to
         # compare.
-        echo -e "commits (in order tested):\n--------------------------\n" >> "$fname"
-        values=${fname}.values
-        measurement_fname=${fname}.tmp
+        combined="measurement_$(git rev-list HEAD -n $last_commits | tail -n1)_$(git rev-list HEAD -n 1 | head -n1)"
+        tmp_values="$combined.values"
+        echo -e "commits (in order tested):\n--------------------------\n" > "$combined"
         commits=$(git rev-list HEAD -n "$last_commits")
         first=true
         empty_pad=""
         for commit in $commits; do
             git checkout "$commit"
-            git log -n 1 --pretty=oneline >> "$fname"
+            git log -n 1 --pretty=oneline >> "$combined"
+            # do measurement for this commit
             startMeasurement
+            # now merge it:
             while read -r line; do
                 key=${line%%=*}
                 value=$(printf "%-20s" "${line##*=}")
                 if [ "$first" = true ] ; then
-                    printf "%-30s%s\n" "$key" "$value" >> "$values"
+                    printf "%-30s%s\n" "$key" "$value" >> "$tmp_values"
                 else
                     if grep "$key" "$values"; then
-                        sed -ri "s/($key.*)/\1$value/g" "$values"
+                        sed -ri "s/($key.*)/\1$value/g" "$tmp_values"
                     else
                         # add a new row, using $empty_pad to get the column in the right place
-                        printf "%-30s%s%s\n" "$key" "$empty_pad" "$value" >> "$values"
+                        printf "%-30s%s%s\n" "$key" "$empty_pad" "$value" >> "$tmp_values"
                     fi
                 fi
             done < "$measurement_fname"
             first=false
             empty_pad=$(printf "%s%-20s" "$empty_pad" "")
         done
-        echo -e "\nmeasurements:\n-------------\n" >> "$fname"
-        cat "$values" >> "$fname"
-        rm "$values"
-        rm "$measurement_fname"
+        echo -e "\nmeasurements:\n-------------\n" >> "$combined"
+        cat "$tmp_values" >> "$combined"
+        # tidy up
+        rm "$tmp_values"
         # revert checkout
         git checkout "$current_commit"
     fi
     # in either case, echo the file:
     echo ""
-    cat "$fname"
+    cat "$measurement_fname"
     exit 0
 else
     set_jar "core"
--- matching-core/src/main/java/com/graphhopper/matching/util/Measurement.java
+++ matching-core/src/main/java/com/graphhopper/matching/util/Measurement.java
@@ -15,7 +15,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package com.graphhopper.matching.tools;
+package com.graphhopper.matching.util;
 
 import com.graphhopper.GHRequest;
 import com.graphhopper.GHResponse;
