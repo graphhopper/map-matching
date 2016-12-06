9d7865733f30a7978b2d886c8b9afbb4832b3cdd
kodonnell <kodonnell@users.noreply.github.com>
2016-12-07 07:33:14 +1300

    tidied up doco a bit

--- map-matching.sh
+++ map-matching.sh
@@ -32,11 +32,26 @@ elif [ "$1" = "action=test" ]; then
     # return exit code of mvn
     exit $?
 elif [ "$1" = "action=measurement" ]; then
+    # the purpose of this is to run the com.graphhopper.matching.util.Measurement suite, which outputs 
+    # measurement files locally. Usage is either
+    #
+    #   ./map-matching.sh action=measurement map.osm.pbf
+    #
+    # which just compiles the current source and exports the measurement file, or
+    #
+    #   ./map-matching.sh action=measurement map.osm.pbf 3
+    #
+    # which (in this case) runs the measurement suite for the last 3 commits. (This involves checking them
+    # out and building one-by-one, which is quite slow.)
+    # TODO: cache some of the builds/tests to speed things up if the developer's looking for quick feedback
+    # TODO: figure out a way to genericise this - see GH PR 894
+    # TODO: add an error quantification (e.g. std) to the measurement class (inc. in GH), and add it to the outputs (inc. plots)
+    
     set_jar "core"
     ARGS="config=$CONFIG graph.location=$GRAPH datareader.file=$2 prepare.ch.weightings=fastest graph.flag_encoders=car prepare.min_network_size=10000 prepare.min_oneway_network_size=10000"
     current_branch=$(git rev-parse --abbrev-ref HEAD)
     function startMeasurement {
-        echo -e "\nperforming measurement for commit $current_commit"
+        # runs a measurement for the current code base
         mvn --projects matching-core -DskipTests=true clean install assembly:single
         measurement_fname="measurement$(date +%Y-%m-%d_%H_%M_%S).properties"
         "$JAVA" $JAVA_OPTS -cp "$JAR" com.graphhopper.matching.util.Measurement $ARGS measurement.location="$measurement_fname"
@@ -44,15 +59,15 @@ elif [ "$1" = "action=measurement" ]; then
 
     # use all <last_commits> versions starting from HEAD
     last_commits=$3
-    if [ -z "$last_commits" ]; then
+    if [ -z "$last_commits" ] || [ "$last_commits" -eq 1 ]; then
         startMeasurement
         echo ""
         cat "$measurement_fname"
         exit 0
     else
-        # the following checks out the last commits, and tests each one. The code looks a bit
+        # as above, let's check out, build, and test each of the last commits. The code looks a bit
         # messier than that, as we merge the results into a single file (to make it easier to
-        # compare.
+        # comparem measurements)
         combined="measurement_$(git log --format="%h" | head -n $last_commits | tail -n1)_$(git log --format="%h" | head -n1).properties"
         tmp_values="$combined.values"
         rm -f "$tmp_values"
@@ -75,12 +90,15 @@ elif [ "$1" = "action=measurement" ]; then
                 key=${line%%=*}
                 value=$(printf "%-20s" "${line##*=}")
                 if [ "$first" = true ] ; then
+                    # first commit, so print key and value
                     printf "%-30s%s\n" "$key" "$value" >> "$tmp_values"
                 else
                     if grep "$key" "$tmp_values" > /dev/null; then
+                        # second (or later) commit, in which key already exists, so we simply append it
+                        # TODO: this may fail if e.g. a key exists in commit 1, not in commit 2, and then again in commit 3 ... pretty unlikely
                         sed -ri "s/($key.*)/\1$value/g" "$tmp_values"
                     else
-                        # add a new row, using $empty_pad to get the column in the right place
+                        # second (or later) commit, in which a new key has appeeard - so add a new line
                         printf "%-30s%s%s\n" "$key" "$empty_pad" "$value" >> "$tmp_values"
                     fi
                 fi
@@ -93,7 +111,7 @@ elif [ "$1" = "action=measurement" ]; then
         echo "$subheader" >> "$combined"
         cat "$tmp_values" >> "$combined"
 
-        # show as is:
+        # show to user now:
         echo ""
         cat "$combined"
 
