6fdd0456ff681ba0919955af53291f907dec8c29
1kastner <1kastner@users.noreply.github.com>
2018-10-28 08:56:28 +0100

    Update start_server.bat

--- start_server.bat
+++ start_server.bat
@@ -1,3 +1,2 @@
-docker build -t graphhopper-map-matching .
-docker run -p 8989:8989 -t graphhopper-map-matching
-
+CALL docker build -t graphhopper-map-matching .
+CALL docker run -p 8989:8989 -t graphhopper-map-matching
