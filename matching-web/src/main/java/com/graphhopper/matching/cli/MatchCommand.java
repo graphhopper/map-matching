8060471e8b8b998ddb21ae2e48779f1451be2c3c
easbar <easbar.mail@posteo.net>
2020-03-24 16:40:09 +0100

    Use GH core 1.0-pre33

--- matching-web/src/main/java/com/graphhopper/matching/cli/MatchCommand.java
+++ matching-web/src/main/java/com/graphhopper/matching/cli/MatchCommand.java
@@ -4,14 +4,12 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.graphhopper.GraphHopper;
 import com.graphhopper.GraphHopperConfig;
 import com.graphhopper.PathWrapper;
+import com.graphhopper.config.ProfileConfig;
 import com.graphhopper.matching.MapMatching;
 import com.graphhopper.matching.MatchResult;
 import com.graphhopper.matching.Observation;
 import com.graphhopper.matching.gpx.Gpx;
 import com.graphhopper.reader.osm.GraphHopperOSM;
-import com.graphhopper.routing.util.FlagEncoder;
-import com.graphhopper.routing.util.HintsMap;
-import com.graphhopper.routing.weighting.FastestWeighting;
 import com.graphhopper.routing.weighting.Weighting;
 import com.graphhopper.util.*;
 import com.graphhopper.util.gpx.GpxFromInstructions;
@@ -67,15 +65,19 @@ public class MatchCommand extends Command {
         graphHopperConfiguration.putObject("graph.location", "graph-cache");
         GraphHopper hopper = new GraphHopperOSM().init(graphHopperConfiguration);
         System.out.println("loading graph from cache");
-        hopper.load(graphHopperConfiguration.getString("graph.location", "graph-cache"));
-
+        // since we are loading GraphHopper from disk we more or less have to guess the configuration
         String vehicle = args.getString("vehicle");
-        FlagEncoder encoder = Helper.isEmpty(vehicle) ? hopper.getEncodingManager().fetchEdgeEncoders().get(0) : hopper.getEncodingManager().getEncoder(vehicle);
+        if (Helper.isEmpty(vehicle))
+            vehicle = hopper.getEncodingManager().fetchEdgeEncoders().get(0).toString();
+
         // Penalizing inner-link U-turns only works with fastest weighting, since
         // shortest weighting does not apply penalties to unfavored virtual edges.
-        HintsMap hintsMap = new HintsMap().setWeighting("fastest").setVehicle(encoder.toString()).putObject(MAX_VISITED_NODES, args.getInt("max_visited_nodes"));
-        Weighting weighting = new FastestWeighting(encoder);
-        MapMatching mapMatching = new MapMatching(hopper, hintsMap);
+        String weightingStr = "fastest";
+        hopper.setProfiles(new ProfileConfig("profile").setVehicle(vehicle).setWeighting(weightingStr).setTurnCosts(false));
+        hopper.load(graphHopperConfiguration.getString("graph.location", "graph-cache"));
+
+        PMap hints = new PMap().putObject(MAX_VISITED_NODES, args.get("max_visited_nodes"));
+        MapMatching mapMatching = new MapMatching(hopper, hints);
         mapMatching.setTransitionProbabilityBeta(args.getDouble("transition_probability_beta"));
         mapMatching.setMeasurementErrorSigma(args.getInt("gps_accuracy"));
 
@@ -85,6 +87,8 @@ public class MatchCommand extends Command {
         Translation tr = new TranslationMap().doImport().getWithFallBack(Helper.getLocale(args.getString("instructions")));
         final boolean withRoute = !args.getString("instructions").isEmpty();
         XmlMapper xmlMapper = new XmlMapper();
+
+        Weighting weighting = hopper.createWeighting(hopper.getProfiles().get(0), hints);
 
         for (File gpxFile : args.<File>getList("gpx")) {
             try {
